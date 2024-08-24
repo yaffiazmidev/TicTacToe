fad53958b598fe640f7e126f9c1a988cc0417731
Yaffi Azmi <44551686+yaffiazmidev@users.noreply.github.com>
2024-08-24 16:18:07 +0700

    Initial commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by DENAZMI on 24/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
